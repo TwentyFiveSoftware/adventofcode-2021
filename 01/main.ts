014d93c1a7c42c1c2665079fc6cac7b7fa0462c2
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-12-02 12:40:17 +0100

    day 1

--- 01/main.ts
+++ 01/main.ts
@@ -1,4 +1,23 @@
 import * as fs from 'fs';
 
-const input = fs.readFileSync(`${__dirname}/input.txt`);
-console.log(input.toString());
+const input: string = fs.readFileSync(`${__dirname}/input.txt`).toString();
+const numbers: number[] = input.split('\n').map(Number);
+
+
+const increasedNumberReducer = (previousValue: number, currentValue: number, currentIndex: number, currentArray: number[]) =>
+    previousValue + (currentArray[currentIndex - 1] < currentValue ? 1 : 0);
+
+
+// PART 1
+const increasedMeasurements = numbers.reduce(increasedNumberReducer, 0);
+
+console.log('PART 1:', increasedMeasurements);
+
+
+// PART 2
+const increasedMeasurementSums = numbers
+    .map((n, i) => n + numbers[i + 1] + numbers[i + 2])
+    .filter(n => !isNaN(n))
+    .reduce(increasedNumberReducer, 0);
+
+console.log('PART 2:', increasedMeasurementSums);
